0fa48628bdb6f4158cdd75d6546632bd9d8f6e34
Alae Jaaouani <alaejaaouani2@gmail.com>
2026-03-26 17:12:43 +0100

    Sécurité : Restriction de l'accès au dashboard uniquement aux membres connectés

--- home_auth/views.py
+++ home_auth/views.py
@@ -2,32 +2,33 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 def login_view(request):
     if request.method == 'POST':
         email_user = request.POST.get('email')
         password_user = request.POST.get('password')
 
-        # 1. On cherche d'abord l'utilisateur par son email
+        
         try:
             user_obj = User.objects.get(email=email_user)
-            # On récupère le vrai username (dans ton cas : 'admin')
+            
             username_to_auth = user_obj.username
         except User.DoesNotExist:
-            # Si l'email n'existe pas, on met une valeur qui échouera à l'auth
+            
             username_to_auth = None
 
-        # 2. On tente l'authentification avec le vrai username trouvé
+        
         user = authenticate(request, username=username_to_auth, password=password_user)
 
         if user is not None:
             login(request, user)
-            # Une fois connecté, on redirige (on peut mettre 'login' en attendant le dashboard)
             messages.success(request, f"Bienvenue {user.username} !")
             return redirect('dashboard') 
         else:
             messages.error(request, "Email ou mot de passe incorrect.")
             
     return render(request, 'login.html')
+@login_required(login_url='login')
 def dashboard_view(request):
     return render(request, 'dashboard.html')
